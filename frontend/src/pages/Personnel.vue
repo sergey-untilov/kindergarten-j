f42a1fbb3b53f72d45a6894dea1c4809c86303d1
unclezeppelin <uncle.zeppelin@gmail.com>
2018-05-16 13:28:06 +0300

    read employees

--- frontend/src/pages/Personnel.vue
+++ frontend/src/pages/Personnel.vue
@@ -63,9 +63,9 @@
     >
       <template slot="items" slot-scope="props">
         <td>{{ props.item.firstName }}</td>
-        <td class="text-xs-right">{{ props.item.LastName }}</td>
-        <td class="text-xs-right">{{ props.item.HireDate }}</td>
-        <td class="text-xs-right">{{ props.item.Salary }}</td>
+        <td class="text-xs-left">{{ props.item.lastName }}</td>
+        <td class="text-xs-right">{{ props.item.hireDate }}</td>
+        <td class="text-xs-right">{{ props.item.salary }}</td>
         <td class="justify-center layout px-0">
           <v-btn icon class="mx-0" @click="editItem(props.item)">
             <v-icon color="green">edit</v-icon>
@@ -94,11 +94,11 @@
           sortable: true,
           value: 'firstName'
         },
-        { text: 'Last Name', value: 'lastName' },
-        { text: 'Hire Date', value: 'hireDate' },
-        { text: 'Salary', value: 'salary' }
+        { text: 'Last Name', value: 'lastName', align: 'left' },
+        { text: 'Hire Date', value: 'hireDate', align: 'right' },
+        { text: 'Salary', value: 'salary', align: 'right' },
+        { text: 'Actions', value: 'actions', align: 'center' }
       ],
-      desserts: [],
       editedIndex: -1,
       editedItem: {
         firstName: '',
@@ -120,7 +120,7 @@
 
     computed: {
       employees() {
-        this.$store.getters.employees
+        return this.$store.getters.employees.data
       },
       formTitle () {
         return this.editedIndex === -1 ? 'New Item' : 'Edit Item'
@@ -134,94 +134,26 @@
     },
 
     created () {
-      this.initialize()
+      this.initialize();
+      this.$store.dispatch('getEmployees');
     },
 
     methods: {
       initialize () {
-        this.desserts = [
-          {
-            name: 'Frozen Yogurt',
-            calories: 159,
-            fat: 6.0,
-            carbs: 24,
-            protein: 4.0
-          },
-          {
-            name: 'Ice cream sandwich',
-            calories: 237,
-            fat: 9.0,
-            carbs: 37,
-            protein: 4.3
-          },
-          {
-            name: 'Eclair',
-            calories: 262,
-            fat: 16.0,
-            carbs: 23,
-            protein: 6.0
-          },
-          {
-            name: 'Cupcake',
-            calories: 305,
-            fat: 3.7,
-            carbs: 67,
-            protein: 4.3
-          },
-          {
-            name: 'Gingerbread',
-            calories: 356,
-            fat: 16.0,
-            carbs: 49,
-            protein: 3.9
-          },
-          {
-            name: 'Jelly bean',
-            calories: 375,
-            fat: 0.0,
-            carbs: 94,
-            protein: 0.0
-          },
-          {
-            name: 'Lollipop',
-            calories: 392,
-            fat: 0.2,
-            carbs: 98,
-            protein: 0
-          },
-          {
-            name: 'Honeycomb',
-            calories: 408,
-            fat: 3.2,
-            carbs: 87,
-            protein: 6.5
-          },
-          {
-            name: 'Donut',
-            calories: 452,
-            fat: 25.0,
-            carbs: 51,
-            protein: 4.9
-          },
-          {
-            name: 'KitKat',
-            calories: 518,
-            fat: 26.0,
-            carbs: 65,
-            protein: 7
-          }
-        ]
       },
 
       editItem (item) {
-        this.editedIndex = this.desserts.indexOf(item)
+        // this.editedIndex = this.desserts.indexOf(item)
+        this.editedIndex = this.$store.getters.employees.data.indexOf(item)
         this.editedItem = Object.assign({}, item)
         this.dialog = true
       },
 
       deleteItem (item) {
-        const index = this.desserts.indexOf(item)
-        confirm('Are you sure you want to delete this item?') && this.desserts.splice(index, 1)
+        // const index = this.desserts.indexOf(item)
+        const index = this.$store.getters.employees.data.indexOf(item)
+        // confirm('Are you sure you want to delete this item?') && this.desserts.splice(index, 1)
+        confirm('Are you sure you want to delete this item?') && this.$store.getters.employees.data.splice(index, 1)
       },
 
       close () {
